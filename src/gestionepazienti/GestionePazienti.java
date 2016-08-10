39a90ec7769a49d905e10da0d7947b0c4cc5af9f
Riccardo <Riccardo@Riccardo-PC>
2016-08-10 16:08:11 +0200

    prova

--- src/gestionepazienti/GestionePazienti.java
+++ src/gestionepazienti/GestionePazienti.java
@@ -15,7 +15,7 @@ public class GestionePazienti {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("prova");
     }
     
 }
