5ec0fe33058cc16fd9d3588cbf9e4083bdb6bb5d
Riccardo <Riccardo@Riccardo-PC>
2016-08-11 19:59:19 +0200

    Merge origin/master
    Conflicts:
    	src/gestionepazienti/GestionePazienti.java

--- src/gestionepazienti/GestionePazienti.java
+++ src/gestionepazienti/GestionePazienti.java
@@ -15,7 +15,7 @@ public class GestionePazienti {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prova11");
+        new MenuPrincipale();
     }
     
 }
