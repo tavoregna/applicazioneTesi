63538a5d67b94970fb2c44cd67b46d8e7af0fbef
Utente <Utente@Utente-PC>
2016-10-07 16:57:38 +0200

    Merge origin/master
    Conflicts:
    	db/Prova1.accdb

--- src/gestionepazienti/NPSUI.java
+++ src/gestionepazienti/NPSUI.java
@@ -6,6 +6,7 @@
 package gestionepazienti;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
